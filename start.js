261a62d73332b8c2781d07273e2d89189997717f
ManoharGolleru <manoharg@buffalo.edu>
2024-01-24 22:40:55 -0500

    link

--- start.js
+++ start.js
@@ -7,8 +7,6 @@ import { Monitor } from "./components/monitor";
 import { ToolBar } from "./components/toolbar";
 import db from "./db";
 import pleaseWait from "./components/wait";
-import "./css/designer.css";
-import "./css/colors.css";
 import { clearAccessChanged } from "./components/access";
 import Globals from "./globals";
 import { PatternList } from "./components/access/pattern";
