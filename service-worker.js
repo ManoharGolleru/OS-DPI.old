7ca727d0213e747241c996d0a95ac2fa7eb0d754
ManoharGolleru <manoharg@buffalo.edu>
2024-01-25 13:13:28 -0500

    s

--- service-worker.js
+++ service-worker.js
@@ -1,53 +1,45 @@
 var GHPATH = "/OS-DPI";
 var APP_PREFIX = "osdpi_";
-var VERSION = APP_VERSION;
+var VERSION = "2024-0-11-16-25-29";
 var URLS = [
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
   `${GHPATH}/index.css`,
   `${GHPATH}/index.js`,
-  `${GHPATH}/xlsx.js`,
+  `${GHPATH}/xlsx.js`
 ];
-
 var CACHE_NAME = APP_PREFIX + VERSION;
-self.addEventListener("fetch", function (/** @type {FetchEvent} */ e) {
-  // console.log("Fetch request : " + e.request.url);
+self.addEventListener("fetch", function(e) {
   const url = new URL(e.request.url);
   if (URLS.includes(url.pathname)) {
     e.respondWith(
-      caches.match(e.request).then(function (request) {
+      caches.match(e.request).then(function(request) {
         if (request) {
-          // console.log("Responding with cache : " + e.request.url);
           return request;
         } else {
-          // console.log("File is not cached, fetching : " + e.request.url);
           return fetch(e.request);
         }
       })
     );
   }
 });
-
-self.addEventListener("install", function (/** @type {ExtendableEvent} */ e) {
+self.addEventListener("install", function(e) {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      // console.log("Installing cache : " + CACHE_NAME);
+    caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll(URLS);
     })
   );
 });
-
-self.addEventListener("activate", function (/** @type {ExtendableEvent} */ e) {
+self.addEventListener("activate", function(e) {
   e.waitUntil(
-    caches.keys().then(function (keyList) {
-      var cacheWhitelist = keyList.filter(function (key) {
+    caches.keys().then(function(keyList) {
+      var cacheWhitelist = keyList.filter(function(key) {
         return key.indexOf(APP_PREFIX);
       });
       cacheWhitelist.push(CACHE_NAME);
       return Promise.all(
-        keyList.map(function (key, i) {
+        keyList.map(function(key, i) {
           if (cacheWhitelist.indexOf(key) === -1) {
-            // console.log("Deleting cache : " + keyList[i]);
             return caches.delete(keyList[i]);
           }
         })
@@ -55,11 +47,10 @@ self.addEventListener("activate", function (/** @type {ExtendableEvent} */ e) {
     })
   );
 });
-
-self.addEventListener("message", (/** @type {MessageEvent} */ event) => {
+self.addEventListener("message", (event) => {
   if (event.data === "SKIP_WAITING") {
-    // I'm missing something in my config to cause the following error
-    // @ts-ignore
+    /** @type {unknown} */
     self.skipWaiting();
   }
 });
+//# sourceMappingURL=service-worker.js.map
