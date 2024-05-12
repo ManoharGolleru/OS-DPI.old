923ca1dfd149b6fd2aee6817e418106edf724b7d
ManoharGolleru <manoharg@buffalo.edu>
2024-05-12 10:37:59 -0400

    UpdateAZURE speech.js

--- components/speech.js
+++ components/speech.js
@@ -1,3 +1,4 @@
+import * as sdk from "microsoft-cognitiveservices-speech-sdk";
 import { strip } from "./display";
 import { TreeBase } from "./treebase";
 import { html } from "uhtml";
@@ -6,29 +7,51 @@ import * as Props from "./props";
 
 class Speech extends TreeBase {
   stateName = new Props.String("$Speak");
-  voiceURI = new Props.Voice("", { label: "Voice" });
-  pitch = new Props.Float(1);
-  rate = new Props.Float(1);
-  volume = new Props.Float(1);
+  voiceURI = new Props.Voice("$VoiceURI", "en-US-GuyNeural"); // Dynamically updated voice URI
+  expressStyle = new Props.String("$ExpressStyle", "friendly"); // Dynamically updated expression style
+  
+
+
+  constructor() {
+    super();
+    this.speechConfig = sdk.SpeechConfig.fromSubscription('25d626bb957d4ae0801f224ed52e04dd', 'eastus');
+    this.audioConfig = sdk.AudioConfig.fromDefaultSpeakerOutput();
+    this.synthesizer = new sdk.SpeechSynthesizer(this.speechConfig, this.audioConfig);
+  }
 
   async speak() {
     const { state } = Globals;
-    const { stateName, voiceURI, pitch, rate, volume } = this.props;
-    const message = strip(state.get(stateName));
-    const voices = await getVoices();
-    const voice =
-      voiceURI && voices.find((voice) => voice.voiceURI == voiceURI);
-    const utterance = new SpeechSynthesisUtterance(message);
-    if (voice) {
-      utterance.voice = voice;
-      utterance.lang = voice.lang;
-    }
-    utterance.pitch = pitch;
-    utterance.rate = rate;
-    utterance.volume = volume;
-    speechSynthesis.cancel();
-    speechSynthesis.speak(utterance);
+
+    // Directly use the strings defined in the constructor for keys
+    const message = state.get("$Speak");  // Use the key directly
+    const voice = state.get("$VoiceURI") || "en-US-GuyNeural"; // Use the key directly
+    const style = state.get("$ExpressStyle") || "friendly";  // Use the key directly
+
+   
+
+    const ssml = `
+    <speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="https://www.w3.org/2001/mstts" xml:lang="en-US">
+      <voice name="${voice}">
+        <mstts:express-as style="${style}">
+          ${message}
+        </mstts:express-as>
+      </voice>
+    </speak>`;
+
+    this.synthesizer.speakSsmlAsync(ssml, result => {
+        if (result.reason === sdk.ResultReason.SynthesizingAudioCompleted) {
+            console.log("Speech synthesized successfully");
+        } else if (result.reason === sdk.ResultReason.Canceled) {
+            const cancellationDetails = sdk.SpeechSynthesisCancellationDetails.fromResult(result);
+            console.error("Speech synthesis canceled:", cancellationDetails.reason, cancellationDetails.errorDetails);
+        }
+    }, error => {
+        console.error("An error occurred:", error);
+    });
   }
+
+
+
 
   template() {
     const { stateName } = this.props;
@@ -38,54 +61,8 @@ class Speech extends TreeBase {
     }
     return this.empty;
   }
-
-  // settings() {
-  //   console.log("speech settings");
-  //   return html`<div class="Speech">
-  //     ${this.stateName.input()} ${this.voiceURI.input()} ${this.pitch.input()}
-  //     ${this.rate.input()} ${this.volume.input()}
-  //   </div>`;
-  // }
 }
+
 TreeBase.register(Speech, "Speech");
 
-/** @type{SpeechSynthesisVoice[]} */
-let voices = [];
 
-/**
- * Promise to return voices
- *
- * @return {Promise<SpeechSynthesisVoice[]>} Available voices
- */
-function getVoices() {
-  return new Promise(function (resolve) {
-    // iOS won't fire the voiceschanged event so we have to poll for them
-    function f() {
-      voices = (voices.length && voices) || speechSynthesis.getVoices();
-      if (voices.length) resolve(voices);
-      else setTimeout(f, 100);
-    }
-    f();
-  });
-}
-
-class VoiceSelect extends HTMLSelectElement {
-  constructor() {
-    super();
-  }
-  connectedCallback() {
-    this.addVoices();
-  }
-
-  async addVoices() {
-    const voices = await getVoices();
-    const current = this.getAttribute("value");
-    for (const voice of voices) {
-      const item = html.node`<option value=${voice.voiceURI} ?selected=${
-        voice.voiceURI == current
-      }>${voice.name}</option>`;
-      this.add(/** @type {HTMLOptionElement} */ (item));
-    }
-  }
-}
-customElements.define("select-voice", VoiceSelect, { extends: "select" });
