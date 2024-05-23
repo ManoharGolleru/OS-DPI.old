cb712ce7b569397a168e1f78e466e05d715cef3d
ManoharGolleru <manoharg@buffalo.edu>
2024-05-23 12:24:23 -0400

    Update button.js

--- components/button.js
+++ components/button.js
@@ -3,20 +3,45 @@ import { TreeBase } from "./treebase";
 import * as Props from "./props";
 import { styleString } from "./style";
 import "css/button.css";
+import { Subject } from "rxjs";
+
+const keyEvent$ = new Subject(); // RxJS Subject to handle key events
 
 class Button extends TreeBase {
   label = new Props.String("click me");
   name = new Props.String("button");
   background = new Props.Color("");
   scale = new Props.Float(1);
+  isActive = new Props.Boolean(false); // State to track active status
+
+  constructor() {
+    super();
+    // Ensure isActive is set to false initially
+    this.isActive = false;
+    this.subscription = keyEvent$.subscribe(this.handleKeyEvent.bind(this));
+  }
+
+  handleKeyEvent(event) {
+    const { name } = this.props;
+    // Toggle active state based on key events
+    if (event.access && event.access.name === name) {
+      if (event.type === "keydown") {
+        this.isActive = true;
+      } else if (event.type === "keyup") {
+        this.isActive = false;
+      }
+      this.update();
+    }
+  }
 
   template() {
     const style = styleString({ backgroundColor: this.props.background });
     const { name, label } = this.props;
+    const isActive = this.isActive; // Get the current state
     return this.component(
       {},
       html`<button
-        class="button"
+        class=${`button ${isActive ? "active" : ""}`}
         name=${name}
         style=${style}
         .dataset=${{
@@ -25,6 +50,8 @@ class Button extends TreeBase {
           ComponentName: this.props.name,
           ComponentType: this.constructor.name,
         }}
+        @focus=${() => keyEvent$.next({ type: 'keydown', access: { name: name }})}
+        @blur=${() => keyEvent$.next({ type: 'keyup', access: { name: name }})}
       >
         ${label}
       </button>`
@@ -34,5 +61,10 @@ class Button extends TreeBase {
   getChildren() {
     return [];
   }
+
+  disconnectedCallback() {
+    this.subscription.unsubscribe();
+  }
 }
 TreeBase.register(Button, "Button");
+
