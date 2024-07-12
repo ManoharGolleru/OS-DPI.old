5ad9eabf5b789bf11d5561bc503150856f49360c
ManoharGolleru <manoharg@buffalo.edu>
2024-07-12 11:49:20 -0400

    Update speech.js

--- components/speech.js
+++ components/speech.js
@@ -6,7 +6,7 @@ import * as Props from "./props"; // Correct import
 
 class Speech extends TreeBase {
   stateName = new Props.String("$Speak");
-  voiceURI = new Props.Voice("$VoiceURI", "en-US-DavisNeural"); // Default to Davis
+  voiceURI = new Props.String("$VoiceURI", "en-US-DavisNeural"); // Default to Davis
   expressStyle = new Props.String("$ExpressStyle", "friendly"); // Default expression style
   isSpeaking = false; // Track if currently speaking
 
@@ -44,16 +44,12 @@ class Speech extends TreeBase {
   }
 
   async stopSynthesis() {
-    return new Promise((resolve, reject) => {
-      this.synthesizer.stopSpeakingAsync(() => {
-        console.log("Previous synthesis stopped");
-        this.isSpeaking = false;
-        resolve();
-      }, error => {
-        console.error("Error stopping synthesis", error);
-        this.isSpeaking = false;
-        resolve(); // Resolve even if there's an error to allow new synthesis to start
-      });
+    return new Promise((resolve) => {
+      this.synthesizer.close();
+      this.isSpeaking = false;
+      this.initSynthesizer(); // Reinitialize the synthesizer
+      console.log("Previous synthesis stopped");
+      resolve();
     });
   }
 
@@ -99,21 +95,16 @@ class Speech extends TreeBase {
 
   disconnectedCallback() {
     if (this.isSpeaking) {
-      this.synthesizer.stopSpeakingAsync(() => {
-        console.log("Synthesizer stopped on component disconnect");
-        this.isSpeaking = false;
-      }, error => {
-        console.error("Error stopping synthesizer on component disconnect", error);
-        this.isSpeaking = false;
-      });
+      this.synthesizer.close();
+      this.isSpeaking = false;
+      console.log("Synthesizer stopped on component disconnect");
     }
-    this.synthesizer.close();
   }
 
   template() {
-    const { stateName, voiceURI } = this.props; // Removed expressStyle from here
+    const { stateName } = this.props; // Only check for stateName
     const { state } = Globals;
-    if (state.hasBeenUpdated(stateName) || state.hasBeenUpdated(voiceURI)) {
+    if (state.hasBeenUpdated(stateName)) {
       this.speak();
     }
     return this.empty;
